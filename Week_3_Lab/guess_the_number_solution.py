9f48dd845428b39838c72dbece6e8114df2fc5bd
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-01-27 04:26:14 -0600

    added count and validation to guess_the_number

---
+++ Week_3_Lab/guess_the_number_solution.py
@@ -0,0 +1,59 @@
+import random
+
+correct = 'you guessed correctly!'
+too_low = 'too low'
+too_high = 'too high'
+
+
+def configure_range():
+    """ Set the high and low values for the random number """
+    return 1, 10
+
+
+def generate_secret(low, high):
+    """ Generate a secret number for the user to guess """
+    return random.randint(low, high)
+
+
+def get_guess():
+    """ get user's guess, as an integer number """
+
+    while True: # new
+        try:
+            return int(input('Guess the secret number? '))
+        except Exception as e:
+            print('You may only enter a number.')
+
+
+def check_guess(guess, secret):
+    """ compare guess and secret, return string describing result of comparison """
+
+    if guess == secret:
+        return correct
+    if guess < secret:
+        return too_low
+    if guess > secret:
+        return too_high
+
+
+def main():
+
+    (low, high) = configure_range()
+    secret = generate_secret(low, high)
+
+    count = 0 # new
+
+    while True:
+        guess = get_guess()
+        result = check_guess(guess, secret)
+        count += 1 # new
+        print(result)
+
+        if result == correct:
+            break
+
+    print(f'Thanks for playing the game! Number of guesses: {count}') # new
+
+
+if __name__ == '__main__':
+    main()
