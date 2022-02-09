4fcffc8e1707c86c8b06c9525d11ce7a2569dbd2
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-02-09 02:44:16 -0600

    Added unit tests, comments, and docstrings for camelcase program

--- Week_4_Lab/camelCase_lab4.py
+++ Week_4_Lab/camelCase_lab4.py
@@ -1,28 +1,33 @@
+"""This program asks the user to input a sentence which the program will then camelcase. It uses a variety of checks
+to ensure that improper inputs are not permitted."""
+
 def camel_case(sentence):
 
     string_list = sentence.split() # splits the user string into individual elements in a list
 
     new_list = [] # initializes an empty list
 
-    ignore_list = ['!', '@', '.', '(', ')', '*'] # TODO: update this
-
     for e in string_list:
-        if e not in ignore_list: # TODO: update this
+        if e.isalpha(): # check if each individual element is alphabetical. if it is, append element to the new list
             new_list.append((e.title())) # for every element in the list, capitalize only the first letter
-
-    first_word = new_list[0].lower() 
-    new_list[0] = first_word
-    # these two statements look at the first element in the list and convert it to lowercase
+            first_word = new_list[0].lower() # look at the first element in the list and convert it to lowercase
+            new_list[0] = first_word # use the original first element as the first element in the new list
 
     camel_sentence = "".join(new_list) # this joins all the elements in the list into a single string
 
     return camel_sentence # returns the value to be printed out in the main function
 
 
-def main():
-    sentence = input('Enter your sentence: ')
-    camelcased = camel_case(sentence)
-    print(camelcased)
+def main(): # main function
+
+    sentence = input('Enter your sentence: ') # user input
+
+    while sentence == '' or sentence.isalpha() is False: # if the input is blank or no alphabetical, the program asks user to try again
+        print('You have not entered anything or you entered an invalid input.')
+        sentence = input('Enter your sentence: ')
+
+    camelcased = camel_case(sentence) # camelcases the user input
+    print(f'Your sentence in camelcase: {camelcased}') # print message with camelcased sentence
 
 
 if __name__ == '__main__':
--- Week_4_Lab/test_camelcase_lab4.py
+++ Week_4_Lab/test_camelcase_lab4.py
@@ -1,16 +1,23 @@
+"""This file contains a list of unit tests to ensure that the program functions correctly when handling unusual inputs."""
+
 import camelCase_lab4
 from unittest import TestCase
 
 class TestCamelCase(TestCase):
 
-    def test_camelcase_sentence(self): #
+    def test_camelcase_sentence(self):
 
-        expected_string = 'theQuickBrownFoxJumps' # create a variable to be tested
+        self.assertEqual('theQuickBrownFoxJumps', camelCase_lab4.camel_case('The quick brown fox jumps')) 
+        # tests an expected input
 
-        self.assertEqual(expected_string, camelCase_lab4.camel_case('The quick brown fox jumps'))
+        self.assertEqual('theQuickBrownFoxJumps', camelCase_lab4.camel_case('         The         quick brown fox         jumps')) 
+        # this test is used to ensure that all spaces are ignored
 
-        self.assertEqual(expected_string, camelCase_lab4.camel_case('         The         quick brown fox         jumps'))
+        self.assertEqual('theQuickBrownFoxJumps', camelCase_lab4.camel_case('The . quick ! brown @ fox  ** ^^^&&&`~ `=+ jumps 88'))
+        # this test checks to make sure that special characters are ignored
 
-        self.assertEqual(expected_string, camelCase_lab4.camel_case('The . quick ! brown @ fox jumps **')) # TODO: update this
+        self.assertEqual('', camelCase_lab4.camel_case('😁😛😋🤣'))
+        # this test checks to ensure that if emojis are inputted an empty string is returned
 
-        # TODO: add more tests. Intentionally create incorrect inputs to see if program handles it properly
+        self.assertEqual('', camelCase_lab4.camel_case(''))
+        # this test checks to ensure that an empty string is returned if an empty string is inputted
